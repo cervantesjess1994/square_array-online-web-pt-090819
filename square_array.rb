c57f2b5f2a55f60aaedf9f750d71b7ddf189ec50
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-18 22:38:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,12 +2,10 @@ def square_array(array)
   array=[1,2,3]
   array.each do|n|
     puts n*n
-end
-  array
+  end
 end
   def square_array(new_numbers)
     new_numbers=[9,10,16,25]
     new_numbers.each{|n|n*n}
-end
   new_numbers
 end
