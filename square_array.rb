f3cdd0dd5f62ebf58287d6b6471a5ba775e54543
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-18 22:35:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  array=[1,2,3]
+  array.each do|n|
+    puts n*n
+end
+  array
+end
+  def square_array(new_numbers)
+    new_numbers=[9,10,16,25]
+    new_numbers.each{|n|n*n}
+end
+  new_numbers
 end
